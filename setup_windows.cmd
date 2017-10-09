8a7a117259f0986c8a8802e0522d7d3320e3d1dd
tanakiat.srisaranyakul <tanakiat.srisaranyakul@allianz.com>
2017-10-09 11:56:26 +0700

    add batch file to install package

---
+++ setup_windows.cmd
@@ -0,0 +1,31 @@
+cls
+@echo off
+pip uninstall -y robotframework-appiumlibrary
+python setup.py install
+IF %ERRORLEVEL% NEQ 0 (
+	goto error
+)
+goto success
+
+:error
+echo.
+color 4
+echo !!! 'robotframework-mongolibrary' idxn repo is not installed properly !!!
+echo.
+goto end
+
+:success
+
+@echo off
+cls
+color 2
+echo.
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo !!!                                                                    !!!
+echo !!! 'robotframework-mongolibrary' idxn repo installed successfully !!!
+echo !!!                                                                    !!!
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+echo.
+color
+:end
+pause
